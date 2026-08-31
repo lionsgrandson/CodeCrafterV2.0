1786f8eec685d61c92a96048f131d9b1138cd99c
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2026-08-31 14:03:26 +0300

    Add verified trust and service area section to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Services, Process } from './components/Services'
 import { Portfolio, Testimonials } from './components/Portfolio'
 import { WhyWorkWithMe, FinalCTA, Footer } from './components/Footer'
 import { HomepageSeo } from './components/HomepageSeo'
+import { TrustCoverageSeo } from './components/TrustCoverageSeo'
 import { SeoPage } from './components/SeoPage'
 import { translations } from './lib/translations'
 import { localizePath, type Language } from './lib/seoRoutes'
@@ -116,6 +117,7 @@ export default function App({ pathname, seoPage }: AppProps) {
               <Process />
               <Portfolio />
               <Testimonials />
+              <TrustCoverageSeo />
               <WhyWorkWithMe />
               <FinalCTA />
             </>
